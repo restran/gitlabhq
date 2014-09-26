b8fd95af25cb5950a491db6370c506f71e642fa3
Larry Li <larryli@qq.com>
2014-09-27 01:21:23 +0800

    fix some

--- app/helpers/commits_helper.rb
+++ app/helpers/commits_helper.rb
@@ -116,7 +116,7 @@ module CommitsHelper
   def view_file_btn(commit_sha, diff, project)
     link_to project_blob_path(project, tree_join(commit_sha, diff.new_path)),
             class: 'btn btn-small view-file js-view-file' do
-      raw('View file @') + content_tag(:span, commit_sha[0..6],
+      raw('查看文件 @') + content_tag(:span, commit_sha[0..6],
                                        class: 'commit-short-id')
     end
   end
